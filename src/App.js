11fea6b4f956d67d3206f748933baa6dcb315c1c
Tayipztrk <tayipztrk@icloud.com>
2021-05-24 13:09:40 +0300

    last

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import MainContext from './MainContext';
+import Sidebar from './components/Sidebar';
+import Content from './components/Content';
+import BrandsData from './brand.json';
+import { useEffect, useState } from 'react';
+import Copied from './components/Copied';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Collection from './components/Collection';
 
 function App() {
+
+  let brandsArray = []
+  Object.keys(BrandsData).map(key => {
+    brandsArray.push(BrandsData[key])
+  })
+
+  const [brands, setBrands] = useState(brandsArray)
+  const [selectedBrands, setSelectedBrands] = useState([])
+  const [copied, setCopied] = useState(false)
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCopied(false)
+    }, 500)
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [copied])
+
+  useEffect(() => {
+    setBrands(brandsArray.filter(brand => brand.title.toLowerCase().includes(search)))
+  }, [search])
+
+  useEffect(() => {
+    document.title = 'React BrandColors Demo'
+  }, [])
+
+  const data = {
+    brands,
+    selectedBrands,
+    setSelectedBrands,
+    setCopied,
+    search,
+    setSearch
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <MainContext.Provider value={data}>
+          {copied && <Copied color={copied}/>}
+          <Sidebar/>
+          <Router>
+            <Switch>
+                <Route  path="/" exact>
+                      <Content/>
+                </Route>
+                <Route  path="/collection/:slugs">
+                      <Collection/>
+                </Route>
+            </Switch>
+          </Router>
+        </MainContext.Provider>
+      </>
   );
+
 }
 
 export default App;
